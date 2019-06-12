5ec7b2c49ed42a52b779e684f2708b350de55872
Rohit Misra <rohit.misra@sixt.com>
2019-06-12 21:02:45 +0200

    Adding central office client + selectable options

--- index.js
+++ index.js
@@ -1,22 +1,78 @@
 'use strict';
 var request = require("request");
 var cookieJar = request.jar()
-const req = request.defaults({jar: cookieJar});
-var formData = {};
-formData['CASETYPES[Familienstandsänderung/ Namensänderung]'] = 2;
-formData['step'] = 'WEB_APPOINT_SEARCH_BY_CASETYPES';
+const req = request.defaults({ jar: cookieJar });
 
-var headersData = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/x-www-form-urlencoded'
-  };
 
-var sessionOptions = {
-    url: 'https://www56.muenchen.de/termin/index.php?loc=BB',
-    jar: cookieJar
+var formData = {
+  step: 'WEB_APPOINT_SEARCH_BY_CASETYPES',
+  'CASETYPES[An- oder Ummeldung - Einzelperson]': '0',
+  'CASETYPES[An- oder Ummeldung - Einzelperson mit eigenen Fahrzeugen]': '0',
+  'CASETYPES[An- oder Ummeldung - Familie]': '0',
+  'CASETYPES[An- oder Ummeldung - Familie mit eigenen Fahrzeugen]': '0',
+  'CASETYPES[Familienstandsänderung/ Namensänderung]': '2',
+  'CASETYPES[Eintragung Übermittlungssperre]': '0',
+  'CASETYPES[Meldebescheinigung]': '0',
+  'CASETYPES[Haushaltsbescheinigung]': '0',
+  'CASETYPES[Melderegisterauskunft]': '0',
+  'CASETYPES[Abmeldung (Einzelperson oder Familie)]': '0',
+  'CASETYPES[Antrag Personalausweis]': '0',
+  'CASETYPES[Antrag Reisepass/Expressreisepass]': '0',
+  'CASETYPES[Antrag vorläufiger Reisepass]': '0',
+  'CASETYPES[Antrag oder Verlängerung/Aktualisierung Kinderreisepass]': '0',
+  'CASETYPES[Ausweisdokumente - Familie (Minderjährige und deren gesetzliche Vertreter)]': '0',
+  'CASETYPES[Nachträgliche Anschriftenänderung Personalausweis/Reisepass/eAT]': '0',
+  'CASETYPES[Nachträgliches Einschalten eID / Nachträgliche Änderung PIN]': '0',
+  'CASETYPES[Widerruf der Verlust- oder Diebstahlanzeige von Personalausweis oder Reisepass]': '0',
+  'CASETYPES[Verlust- oder Diebstahlanzeige von Personalausweis]': '0',
+  'CASETYPES[Verlust- oder Diebstahlanzeige von Reisepass]': '0',
+  'CASETYPES[Gewerbeummeldung (Adressänderung innerhalb Münchens)]': '0',
+  'CASETYPES[Gewerbeabmeldung]': '0',
+  'CASETYPES[Führungszeugnis beantragen]': '0',
+  'CASETYPES[Gewerbezentralregisterauskunft beantragen – natürliche Person]': '0',
+  'CASETYPES[Gewerbezentralregisterauskunft beantragen – juristische Person]': '0',
+  'CASETYPES[Bis zu 5 Beglaubigungen Unterschrift]': '0',
+  'CASETYPES[Bis zu 5 Beglaubigungen Dokument]': '0',
+  'CASETYPES[Bis zu 20 Beglaubigungen]': '0',
+  'CASETYPES[Fabrikneues Fahrzeug anmelden (mit deutschen Fahrzeugpapieren und CoC)]': '0',
+  'CASETYPES[Fahrzeug wieder anmelden]': '0',
+  'CASETYPES[Fahrzeug umschreiben von außerhalb nach München]': '0',
+  'CASETYPES[Fahrzeug umschreiben innerhalb Münchens]': '0',
+  'CASETYPES[Fahrzeug außer Betrieb setzen]': '0',
+  'CASETYPES[Saisonkennzeichen beantragen]': '0',
+  'CASETYPES[Kurzzeitkennzeichen beantragen]': '0',
+  'CASETYPES[Umweltplakette/ Feinstaubplakette für Umweltzone beantragen]': '0',
+  'CASETYPES[Adressänderung in Fahrzeugpapiere eintragen lassen]': '0',
+  'CASETYPES[Namensänderung in Fahrzeugpapiere eintragen lassen]': '0',
+  'CASETYPES[Verlust oder Diebstahl der Zulassungsbescheinigung Teil I]': '0'
 };
 
-var terminOptions = { method: 'POST',
+var headersData = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/x-www-form-urlencoded'
+};
+
+var sessionOptionsCentral = {
+  url: 'https://www12.muenchen.de/termin/index.php?cts=10224134=2',
+  jar: cookieJar
+};
+
+var sessionOptionsOthers = {
+  url: 'https://www56.muenchen.de/termin/index.php?loc=BB',
+  jar: cookieJar
+};
+
+var terminOptionsCentral = {
+  method: 'POST',
+  followAllRedirects: true,
+  url: 'https://www12.muenchen.de/termin/index.php?cts=10224134=2',
+  headers: headersData,
+  formData: formData,
+  jar: cookieJar
+};
+
+var terminOptionsOthers = {
+  method: 'POST',
   followAllRedirects: true,
   url: 'https://www56.muenchen.de/termin/index.php?loc=BB',
   headers: headersData,
@@ -24,38 +80,48 @@ var terminOptions = { method: 'POST',
   jar: cookieJar
 };
 
-req.post(terminOptions, function(){
-  req.post(terminOptions, function (error, response, body) {
+req.post(sessionOptionsCentral, function () {
+  req.post(terminOptionsCentral, function (error, response, body) {
     if (error) throw new Error(error);
-      var jsonAppoints = parseResponseBody(body);
-      findNextAppointment(jsonAppoints);
+    var jsonAppoints = parseResponseBodyCentral(body);
+    findNextAppointment(jsonAppoints);
+    req.post(sessionOptionsOthers, function (error, response, body) {
+      req.post(terminOptionsOthers, function (error, response, body) {
+        if (error) throw new Error(error);
+        var jsonAppoints = parseResponseBodyOthers(body);
+        findNextAppointment(jsonAppoints);
+      });
+    });
   });
 });
 
-var parseResponseBody = function (body){
+var parseResponseBodyCentral = function (body) {
   var scriptTagRegex = new RegExp(/jsonAppoints(.*?);/gi);
-    var textToTest =  body;
-    var obj = scriptTagRegex.exec(textToTest);
-    return JSON.parse(obj[0].substring(16,obj[0].length-2))
+  var textToTest = body;
+  var obj = scriptTagRegex.exec(textToTest);
+  return JSON.parse(obj[0].substring(16, obj[0].length - 2))
 }
 
-var findNextAppointment = function (jsonAppoints){
+var parseResponseBodyOthers = function (body) {
+  var scriptTagRegex = new RegExp(/jsonAppoints(.*?);/gi);
+  var textToTest = body;
+  var obj = scriptTagRegex.exec(textToTest);
+  return JSON.parse(obj[0].substring(16, obj[0].length - 2))
+}
+
+var findNextAppointment = function (jsonAppoints) {
   var availAppoints = {};
-  for(var loc in jsonAppoints){
-    var availAppointsInLoc = {}; 
-    for(var dateTag in jsonAppoints[loc].appoints){
-      for(var slot in jsonAppoints[loc].appoints[dateTag]){
-        if(!(dateTag in availAppointsInLoc)){
+  for (var loc in jsonAppoints) {
+    var availAppointsInLoc = {};
+    for (var dateTag in jsonAppoints[loc].appoints) {
+      for (var slot in jsonAppoints[loc].appoints[dateTag]) {
+        if (!(dateTag in availAppointsInLoc)) {
           availAppointsInLoc[dateTag] = new Array();
         }
-        availAppointsInLoc[dateTag].push(jsonAppoints[loc].appoints[dateTag][slot])
+        availAppointsInLoc[dateTag].push(new Date(jsonAppoints[loc].appoints[dateTag][slot]).toDateString());
       }
     }
     availAppoints[jsonAppoints[loc].caption] = availAppointsInLoc;
   }
   console.log(JSON.stringify(availAppoints, null, 3));
 }
-
-
-
-
